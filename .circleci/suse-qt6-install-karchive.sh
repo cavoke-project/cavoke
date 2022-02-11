f67ee47f4178e7fdc9258bbc685bfc9f8ec7bcdd
Alex Kovrigin <a.kovrigin0@gmail.com>
2022-02-12 01:52:46 +0300

    [ci] try fix qt6

---
+++ .circleci/suse-qt6-install-karchive.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# Install ECM
+zypper -n install extra-cmake-modules
+# Prepare KArchive
+git clone https://github.com/KDE/karchive
+mkdir karchive/build
+cd karchive
+git switch -c 5.90.0  # FIXME
+# Build 
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=debug -DCMAKE_INSTALL_PREFIX=/usr/local -DQT_MAJOR_VERSION=6
+make
+make install
+cd /
