c0096a108f18148c26a93ae950b0e0d531609a37
Petr Tsvetkov <petr.tsv@gmail.com>
2022-02-14 02:00:07 +0300

    Set text/plain for health request

--- server/controllers/health_controller.cpp
+++ server/controllers/health_controller.cpp
@@ -3,6 +3,7 @@ void cavoke::server::controllers::HealthController::health(
     const drogon::HttpRequestPtr &req,
     std::function<void(const drogon::HttpResponsePtr &)> &&callback) {
   auto resp = drogon::HttpResponse::newHttpResponse();
+  resp->setContentTypeString("text/plain");
   resp->setBody("OK");
   callback(resp);
 }
