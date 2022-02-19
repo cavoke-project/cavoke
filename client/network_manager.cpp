cf5e6d40ab5a16dc1eda0d70e667640230f28eac
Mark <markthehopeful@gmail.com>
2022-02-19 23:57:05 +0300

    No more Qt6 features! And repeated connections

--- client/network_manager.cpp
+++ client/network_manager.cpp
@@ -1,11 +1,9 @@
 #include "network_manager.h"
 NetworkManager::NetworkManager(QObject *parent) : manager(parent) {
+    connect(&manager, SIGNAL(finished(QNetworkReply *)), this,
+            SLOT(doneTestHealthCheck(QNetworkReply *)));
 }
 void NetworkManager::doTestHealthCheck() {
-    connect(&manager, SIGNAL(finished(QNetworkReply *)), this,
-            SLOT(doneTestHealthCheck(QNetworkReply *)),
-            Qt::SingleShotConnection);  // TODO: This is Qt6 feature only!
-
     manager.get(QNetworkRequest(
         QUrl("https://private-e45526-cavoke.apiary-mock.com/health")));
 }
@@ -13,7 +11,6 @@ void NetworkManager::doTestHealthCheck() {
 void NetworkManager::doneTestHealthCheck(QNetworkReply *reply) {
     QString answer = reply->readAll();
     reply->close();
-    qDebug() << "Entered here";
     reply->deleteLater();
     qDebug() << answer;
 }
