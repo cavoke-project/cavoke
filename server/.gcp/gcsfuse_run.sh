1e06cd5b394f8eddf79b85e273e2d98e6ab53ffa
Alex Kovrigin <a.kovrigin0@gmail.com>
2022-03-26 21:03:37 +0300

    try add gcr dockerfile

---
+++ server/.gcp/gcsfuse_run.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+# Create mount directory for service
+mkdir -p "$MNT_DIR"
+
+echo "Mounting GCS Fuse."
+gcsfuse --debug_gcs --debug_fuse "$BUCKET" "$MNT_DIR"
+echo "Mounting completed."
+
+# Run the web service on container startup. Here we use the gunicorn
+# webserver, with one worker process and 8 threads.
+# For environments with multiple CPU cores, increase the number of workers
+# to be equal to the cores available.
+# Timeout is set to 0 to disable the timeouts of the workers to allow Cloud Run to handle instance scaling.
+exec /usr/local/bin/cavoke_server -p "$PORT" -g "$MNT_DIR" "$@" &
+
+# Exit immediately when one of the background processes terminate.
+wait -n
