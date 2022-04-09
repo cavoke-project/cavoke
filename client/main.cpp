9da0c9a30802cab9e6dd05aad3d8240930f289da
Mark Ipatov <markthehopeful@gmail.com>
2022-04-09 20:21:51 +0300

    settingsview.ui updated, organization info added

--- client/main.cpp
+++ client/main.cpp
@@ -3,6 +3,9 @@
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
+    QCoreApplication::setOrganizationName("Cavoke");
+    QCoreApplication::setOrganizationDomain("cavoke.alexkovrigin.me");
+    QCoreApplication::setApplicationName("Cavoke");
     CavokeClientController c(&a);
     return a.exec();
 }
