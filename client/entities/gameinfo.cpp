85f3bcc8fa862f98b76ac635da91d0343cbef0e7
Mark Ipatov <markthehopeful@gmail.com>
2022-04-24 21:12:02 +0300

    GameInfo updated

--- client/entities/gameinfo.cpp
+++ client/entities/gameinfo.cpp
@@ -4,28 +4,37 @@ GameInfo::GameInfo() = default;
 GameInfo::GameInfo(QString _id,
                    QString _display_name,
                    QString _description,
-                   int _players_num)
+                   int _players_num,
+                   QVector<QString> _role_names)
     : id(std::move(_id)),
       display_name(std::move(_display_name)),
       description(std::move(_description)),
-      players_num(_players_num) {
+      players_num(_players_num),
+      role_names(std::move(_role_names)) {
 }
 void GameInfo::read(const QJsonObject &json) {
-    if (json.contains("id") && json["id"].isString())
-        id = json["id"].toString();
+    if (json.contains(ID) && json[ID].isString())
+        id = json[ID].toString();
 
-    if (json.contains("display_name") && json["display_name"].isString())
-        display_name = json["display_name"].toString();
+    if (json.contains(DISPLAY_NAME) && json[DISPLAY_NAME].isString())
+        display_name = json[DISPLAY_NAME].toString();
 
-    if (json.contains("description") && json["description"].isString())
-        description = json["description"].toString();
+    if (json.contains(DESCRIPTION) && json[DESCRIPTION].isString())
+        description = json[DESCRIPTION].toString();
 
-    if (json.contains("players_num") && json["players_num"].isDouble())
-        players_num = json["players_num"].toInt();
+    if (json.contains(PLAYERS_NUM) && json[PLAYERS_NUM].isDouble())
+        players_num = json[PLAYERS_NUM].toInt();
+
+    if (json.contains(ROLE_NAMES) && json[ROLE_NAMES].isArray()) {
+        for (auto obj : json[ROLE_NAMES].toArray()) {
+            role_names.push_back(obj.toString());
+        }
+    }
 }
 void GameInfo::write(QJsonObject &json) const {
     json["id"] = id;
-    json["display_name"] = display_name;
-    json["description"] = description;
-    json["players_num"] = players_num;
+    json[DISPLAY_NAME] = display_name;
+    json[DESCRIPTION] = description;
+    json[PLAYERS_NUM] = players_num;
+    // Actually, write is not used
 }
