c4fbfb257a57dc28bedf7be673fbed95d9a822d3
Petr Tsvetkov <petr.tsv@gmail.com>
2022-05-11 20:21:40 +0300

    Async io for boost::process (to fix windows bug)

--- server/model/logic/game_logic_manager.cpp
+++ server/model/logic/game_logic_manager.cpp
@@ -4,6 +4,7 @@
 #ifndef __kernel_entry
 #define __kernel_entry
 #endif
+#include <boost/asio.hpp>
 #include <boost/process.hpp>
 #include <utility>
 
@@ -16,29 +17,32 @@ GameLogicManager::GameLogicManager(std::shared_ptr<GamesStorage> games_storage)
 std::string GameLogicManager::invoke_logic(const Game &game,
                                            const std::string &input) {
     namespace bp = boost::process;
-    bp::opstream in;
-    bp::ipstream out;
+    boost::asio::io_service ios;
 
-    bp::child c(game.LOGIC_FILE, bp::start_dir(game.directory),
-                bp::std_out > out, bp::std_in < in);
-    in.write(input.c_str(), static_cast<int>(input.size()));
-    in.flush();
+    auto input_buffer = boost::asio::buffer(input);
+    bp::async_pipe input_pipe(ios);
+    std::vector<char> output_buffer(MAX_GAME_RESPONSE_B);
 
-    in.pipe().close();
+    bp::child c(
+        game.LOGIC_FILE, bp::start_dir(game.directory),
+        bp::std_in<input_pipe, bp::std_out> boost::asio::buffer(output_buffer),
+        ios);
 
-    if (!c.wait_for(std::chrono::milliseconds{500})) {
+    boost::asio::async_write(
+        input_pipe, input_buffer,
+        [&input_pipe](const boost::system::error_code &ec, std::size_t n) {
+            input_pipe.async_close();
+        });
+
+    ios.run();
+
+    if (!c.wait_for(std::chrono::milliseconds{LOGIC_TIMEOUT_MS})) {
+        LOG_ERROR << game.logic_file.string() << " logic executable timeout ("
+                  << LOGIC_TIMEOUT_MS << " ms) exceeded";
         c.terminate();
     }
 
-    std::string output;
-    std::string line;
-
-    while (std::getline(out, line)) {
-        output += line;
-        output.push_back('\n');
-    }
-
-    return output;
+    return std::string(output_buffer.begin(), output_buffer.end());
 }
 
 cavoke::ValidationResult GameLogicManager::validate_settings(
