103e0c2d3aadde6b11ff931dc0ff17159811ebd6
Petr Tsvetkov <petr.tsv@gmail.com>
2022-05-16 13:44:30 +0300

    Add new methods to statistics_manager

--- server/model/statistics/statistics_manager.cpp
+++ server/model/statistics/statistics_manager.cpp
@@ -16,28 +16,79 @@ StatisticsManager::GameStatistics StatisticsManager::get_game_statistics(
             get_total_time_played(game_id), get_total_games_played(game_id)};
 }
 
+StatisticsManager::UserGameStatistics
+StatisticsManager::get_user_game_statistics(const std::string &user_id,
+                                            const std::string &game_id) {
+    return {get_user_time_played(user_id, game_id),
+            get_user_games_played(user_id, game_id),
+            get_user_win_rate(user_id, game_id)};
+}
+
+StatisticsManager::UserStatistics StatisticsManager::get_user_statistics(
+    const std::string &user_id) {
+    return {
+        get_user_total_time_played(user_id),
+            get_user_total_games_played(user_id)
+    }
+}
+
 int StatisticsManager::get_avg_duration(const std::string &game_id) {
-    auto duration_result = drogon::app().getDbClient()->execSqlSync(
+    auto db_result = drogon::app().getDbClient()->execSqlSync(
         "select get_average_session_time_sec($1::varchar) as res;", game_id);
-    return duration_result[0]["res"].as<int>();
+    return db_result[0]["res"].as<int>();
 }
 
 int StatisticsManager::get_total_time_played(const std::string &game_id) {
-    auto duration_result = drogon::app().getDbClient()->execSqlSync(
+    auto db_result = drogon::app().getDbClient()->execSqlSync(
         "select get_total_time_sec($1::varchar) as res;", game_id);
-    return duration_result[0]["res"].as<int>();
+    return db_result[0]["res"].as<int>();
 }
 
 int StatisticsManager::get_total_games_played(const std::string &game_id) {
-    auto duration_result = drogon::app().getDbClient()->execSqlSync(
+    auto db_result = drogon::app().getDbClient()->execSqlSync(
         "select get_sessions_num($1::varchar) as res;", game_id);
-    return duration_result[0]["res"].as<int>();
+    return db_result[0]["res"].as<int>();
 }
 
 int StatisticsManager::get_avg_players_cnt(const std::string &game_id) {
-    auto duration_result = drogon::app().getDbClient()->execSqlSync(
+    auto db_result = drogon::app().getDbClient()->execSqlSync(
         "select get_average_players_num($1::varchar) as res;", game_id);
-    return duration_result[0]["res"].as<int>();
+    return db_result[0]["res"].as<int>();
+}
+
+int StatisticsManager::get_user_total_time_played(const std::string &user_id) {
+    auto db_result = drogon::app().getDbClient()->execSqlSync(
+        "select get_cavoke_time_sec_for_user($1::uuid) as res;", user_id);
+    return db_result[0]["res"].as<int>();
+}
+
+int StatisticsManager::get_user_total_games_played(const std::string &user_id) {
+    auto db_result = drogon::app().getDbClient()->execSqlSync(
+        "select get_cavoke_sessions_num_for_user($1::uuid) as res;", user_id);
+    return db_result[0]["res"].as<int>();
+}
+
+double StatisticsManager::get_user_win_rate(const std::string &user_id,
+                                            const std::string &game_id) {
+    auto db_result = drogon::app().getDbClient()->execSqlSync(
+        "select get_win_rate($1::varchar, $2::uuid) as res;", game_id, user_id);
+    return db_result[0]["res"].as<double>();
+}
+
+int StatisticsManager::get_user_games_played(const std::string &user_id,
+                                             const std::string &game_id) {
+    auto db_result = drogon::app().getDbClient()->execSqlSync(
+        "select get_sessions_num_for_user($1::varchar, $2::uuid) as res;",
+        game_id, user_id);
+    return db_result[0]["res"].as<int>();
+}
+
+int StatisticsManager::get_user_time_played(const std::string &user_id,
+                                            const std::string &game_id) {
+    auto db_result = drogon::app().getDbClient()->execSqlSync(
+        "select get_total_time_sec_for_user($1::varchar, $2::uuid) as res;",
+        game_id, user_id);
+    return db_result[0]["res"].as<int>();
 }
 
 }  // namespace cavoke::server::model
--- server/model/statistics/statistics_manager.h
+++ server/model/statistics/statistics_manager.h
@@ -21,18 +21,50 @@ public:
         int total_games_played;
     };
 
+    struct UserGameStatistics {
+        int time_played_sec;
+        int games_played;
+        double win_rate;
+    };
+
+    struct UserStatistics {
+        int total_time_played_sec;
+        int total_games_played;
+    };
+
     GameStatistics get_game_statistics(const std::string &game_id);
+    UserGameStatistics get_user_game_statistics(const std::string &user_id,
+                                                const std::string &game_id);
+    StatisticsManager::UserStatistics get_user_statistics(
+        const std::string &user_id);
 
 private:
     int get_avg_duration(const std::string &game_id);
     int get_avg_players_cnt(const std::string &game_id);
     int get_total_time_played(const std::string &game_id);
     int get_total_games_played(const std::string &game_id);
+    int get_user_total_time_played(const std::string &user_id);
+    int get_user_total_games_played(const std::string &user_id);
+    double get_user_win_rate(const std::string &user_id,
+                             const std::string &game_id);
+    int get_user_games_played(const std::string &user_id,
+                              const std::string &game_id);
+    int get_user_time_played(const std::string &user_id,
+                             const std::string &game_id);
 };
 
 NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(StatisticsManager::GameStatistics,
                                    average_duration_sec,
                                    average_players_count,
+                                   total_time_played_sec,
+                                   total_games_played);
+
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(StatisticsManager::UserGameStatistics,
+                                   time_played_sec,
+                                   games_played,
+                                   win_rate);
+
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(StatisticsManager::UserStatistics,
                                    total_time_played_sec,
                                    total_games_played);
 
