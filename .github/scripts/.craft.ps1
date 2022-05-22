e5cead13961b86681429044fd46da3204dd1e0a7
Alex Kovrigin <a.kovrigin0@gmail.com>
2022-05-23 01:23:53 +0300

    try kde craft

---
+++ .github/scripts/.craft.ps1
@@ -0,0 +1,14 @@
+if ($IsWindows) {
+    $python = (Get-Command py).Source
+} else {
+    $python = (Get-Command python3).Source
+}
+$command = @("${env:HOME}/craft/CraftMaster/CraftMaster/CraftMaster.py",
+             "--config", "${env:GITHUB_WORKSPACE}/.craft.ini",
+             "--config-override", "${env:GITHUB_WORKSPACE}/.github/workflows/craft_override.ini",
+             "--target", "${env:CRAFT_TARGET}",
+             "--variables", "WORKSPACE=${env:HOME}/craft") + $args
+
+Write-Host "Exec: ${python} ${command}"
+
+& $python @command
