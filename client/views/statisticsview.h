b06b44ab78a7c1ea938f6cca5be52b9ec48b70e9
Mark Ipatov <markthehopeful@gmail.com>
2022-05-22 19:24:52 +0300

    First working statistics version

--- client/views/statisticsview.h
+++ client/views/statisticsview.h
@@ -3,6 +3,8 @@
 
 #include <QMainWindow>
 #include "entities/gameinfo.h"
+#include "entities/usergamestatistics.h"
+#include "entities/userstatistics.h"
 
 namespace Ui {
 class StatisticsView;
@@ -16,14 +18,20 @@ public:
 
 public slots:
     void gotGamesListUpdate(const std::vector<GameInfo> &newGamesList);
+    void gotUserStatisticsUpdate(const UserStatistics &userStatistics);
+    void gotUserGameStatisticsUpdate(
+        const UserGameStatistics &userGameStatistics);
 
 signals:
     void shownStartView();
-//    void joinedGame(const QString &inviteCode);
+    void requestedRefresh();
+    void statisticsGameChanged(const QString &gameId);
 
 private slots:
     void on_backButton_clicked();
-//    void on_joinGameButton_clicked();
+    void on_refreshButton_clicked();
+    void displayEmpty();
+    void repeaterCurrentIndexChanged(int index);
 
 private:
     Ui::StatisticsView *ui;
