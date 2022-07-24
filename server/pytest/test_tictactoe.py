06b9fa17beb5abc1f87b91be5a6458fd747441a3
Alex Kovrigin <a.kovrigin0@gmail.com>
2022-07-25 01:29:11 +0300

    [test] add sync test that should fail

--- server/pytest/test_tictactoe.py
+++ server/pytest/test_tictactoe.py
@@ -1,3 +1,5 @@
+import logging
+import threading
 import time
 import uuid
 from typing import List
@@ -50,6 +52,60 @@ def test_simple_game():
             time.sleep(0.1)  # for synchronizing moves
 
         final_session: default_api.SessionInfo = api_instance.session_info(session.session_id, user_id=bob_id)
+        final_state: default_api.GameState = api_instance.get_update(session.session_id, user_id=alice_id)
+        assert final_session.session_id == session.session_id
+        assert final_session.status == 2
+        assert final_state.state == 'OXOXXOXOX'
+
+
+def test_transaction_isolation_on_game_end():
+    with cavoke_openapi_client.ApiClient(pytest.server_config) as api_client:
+        api_instance = default_api.DefaultApi(api_client)
+        alice_id = str(uuid.uuid4())
+        bob_id = str(uuid.uuid4())
+
+        session: default_api.SessionInfo = api_instance.create_session(tictactoe_game_id, user_id=alice_id)
+        assert session.game_id == tictactoe_game_id
+
+        s2 = api_instance.join_session(session.session_id, user_id=bob_id)
+        assert s2.status == 0
+
+        api_instance.start_session(session.session_id, user_id=alice_id)
+
+        # Move
+        moves = [
+            (0, 'X 4'),
+            (1, 'O 2'),
+            (0, 'X 8'),
+            (1, 'O 0'),
+            (0, 'X 1'),
+            (1, 'O 7'),
+            (0, 'X 3'),
+            (1, 'O 5')
+        ]
+        for player_id, move in moves:
+            assert api_instance.session_info(session.session_id, user_id=bob_id).status == 1
+            api_instance.send_move(session.session_id, user_id=bob_id if player_id else alice_id,
+                                   game_move=default_api.GameMove(move))
+            time.sleep(0.1)  # for synchronizing moves
+
+        do_spamming = True
+
+        def spam_x6_player_1():
+            logging.debug('Commencing spamming')
+            while do_spamming:
+                api_instance.send_move(session.session_id, user_id=bob_id, game_move=default_api.GameMove('X 6'), async_req=True)
+
+        t_spam = threading.Thread(target=spam_x6_player_1)
+        t_spam.start()
+
+        time.sleep(0.2)
+
+        api_instance.send_move(session.session_id, user_id=alice_id, game_move=default_api.GameMove('X 6'))
+
+        do_spamming = False
+
+        final_session: default_api.SessionInfo = api_instance.session_info(session.session_id, user_id=alice_id)
         final_state: default_api.GameState = api_instance.get_update(session.session_id, user_id=alice_id)
         assert final_session.session_id == session.session_id
         assert final_session.status == 2
